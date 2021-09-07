41694e6c61458ee6bab0724a4da4eee7ed3086bc
jonki324 <jonki324@gmail.com>
2021-09-08 01:53:12 +0900

    json-server追加

---
+++ mock/api/todos.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    id: 1,
+    body: 'Todo01',
+    isDone: false,
+  },
+  {
+    id: 2,
+    body: 'Todo02',
+    isDone: true,
+  },
+  {
+    id: 3,
+    body: 'Todo03',
+    isDone: true,
+  },
+  {
+    id: 4,
+    body: 'Todo04',
+    isDone: false,
+  },
+  {
+    id: 5,
+    body: 'Todo05',
+    isDone: false,
+  },
+];
